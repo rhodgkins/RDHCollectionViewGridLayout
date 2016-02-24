4d48dbcd8cdcc9b901ca3398b05f1ea8e34ba6e3
Rich Hodgkins <rhodgkins@gmail.com>
2016-02-24 22:32:48 +0000

    Added apple tv target

---
+++ Examples/AppleTVDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppleTVDemo
+//
+//  Created by Richard Hodgkins on 24/02/2016.
+//  Copyright © 2016 Rich H. All rights reserved.
+//
+
+import UIKit
+
+import RDHCollectionViewGridLayout
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
